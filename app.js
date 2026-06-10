83d3b9a928da9807f52f6aed319ce2ed89bf5e56
The Icarus Archives <283800657+theicarusarchives@users.noreply.github.com>
2026-06-10 18:40:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 window.addEventListener("DOMContentLoaded", () => {
+
+  document.addEventListener("mousemove", (e) => {
+  const dot = document.createElement("div");
+  dot.className = "cursor-trail";
+
+  dot.style.left = e.clientX + "px";
+  dot.style.top = e.clientY + "px";
+
+  document.body.appendChild(dot);
+
+  setTimeout(() => {
+    dot.remove();
+  }, 300);
+});
   
 let mediaRecorder;
 let audioChunks = [];
