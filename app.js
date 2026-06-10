892afeb12a7ec242b595cf3a358915ce73a9443b
The Icarus Archives <283800657+theicarusarchives@users.noreply.github.com>
2026-06-10 22:45:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 window.addEventListener("load", () => {
   const canvas = document.getElementById("starfield");
-const canvas = document.getElementById("starfield");
 if (!canvas) return;
 
 const ctx = canvas.getContext("2d");
